9b5177ff3bf468f8fd528545517428ef07e01fff
kylobite <kylolink@gmail.com>
2013-11-30 14:08:10 -0600

    Added logo and stuff

--- public/javascripts/effects.js
+++ public/javascripts/effects.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $(".btn, .modal").on("click", function() {
-        if (!$(this).is(".btn.on")) {
+        if (!$(this).is(".btn.on") && !$(this).is(":hidden")) {
+            if ($(this).is(":hidden")) console.log(1)
             $(".modal, .app, .btn").toggleClass("on");
         }
     });
